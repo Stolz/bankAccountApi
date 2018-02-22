ec0f43f5f38ce13ea5d947b80a474e0bc89a9dd9
Stolz <Stolz@users.noreply.github.com>
2018-02-22 23:56:07 +0800

    Added database migrations

---
+++ lumen/database/migrations/0000_00_00_000004_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->string('number');
+            $table->primary('number');
+            $table->string('owner');
+            $table->decimal('balance', 12, 2)->default(0);
+            $table->boolean('closed')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
